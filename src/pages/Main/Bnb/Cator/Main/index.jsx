031b7b3db10d2bba76c801e7c0ac2b0bf51dfec0
jaesincho <jaesin.cho@dalicious.co>
2023-01-27 15:30:31 +0900

    Main Bottom Navigator 케이터링 신청하기 페이지 작성중

--- src/pages/Main/Bnb/Cator/Main/index.jsx
+++ src/pages/Main/Bnb/Cator/Main/index.jsx
@@ -1,16 +1,61 @@
-import React from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import React from 'react';
+// import {SafeAreaView, Text, View} from 'react-native';
+import styled from 'styled-components/native';
+import Typography from '../../../../../components/Typography';
 
 export const PAGE_NAME = 'P_MAIN__BNB__CATOR';
 
 const Pages = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>{PAGE_NAME}</Text>
-      </View>
-    </SafeAreaView>
-  )
-}
+    <SafeAreaContainer>
+      <Container>
+        <ImageWrap>{/* <CaterImage source={require('../~~')} /> */}</ImageWrap>
+
+        <MetadataWrap></MetadataWrap>
+      </Container>
+    </SafeAreaContainer>
+  );
+};
 
 export default Pages;
+
+{
+  /* <BackgroundImageBox source={LogoBackground} resizeMode="cover"/> */
+}
+// export const LogoBackground = require('../assets/images/loginBackground.png');
+
+const SafeAreaContainer = styled.SafeAreaView`
+  height: 100%;
+  width: 100%;
+`;
+
+const Container = styled.View`
+  align-items: center;
+`;
+
+const ImageWrap = styled.View``;
+
+const CaterImage = styled.Image``;
+
+// const CaterImage = styled.
+
+const MetadataWrap = styled.View``;
+
+const ToastMessageLarge = styled(Typography).attrs({text: 'Body06R'})`
+  overflow: hidden;
+  color: ${({theme}) => theme.colors.neutral[0]};
+`;
+
+const LargeTitle = styled(Typography).attrs({text: 'Body06R'})`
+  overflow: hidden;
+  color: ${({theme}) => theme.colors.neutral[0]};
+`;
+
+const Body05R = styled(Typography).attrs({text: 'Body06R'})`
+  overflow: hidden;
+  color: ${({theme}) => theme.colors.neutral[0]};
+`;
+
+// const UrlPressable = styled.pressable``;
+
+// const
