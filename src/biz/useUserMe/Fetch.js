aa67b2e16a067151bdc91f12ae3720d55692d609
KyoungTae <117700295+Moyahojang@users.noreply.github.com>
2023-03-10 16:49:23 +0900

    카드 삭제 메소드 변경

--- src/biz/useUserMe/Fetch.js
+++ src/biz/useUserMe/Fetch.js
@@ -1,96 +1,93 @@
-import { fetchJson } from "../../utils/fetch";
+import {fetchJson} from '../../utils/fetch';
 
 export async function userMe() {
   const fetchRes = await fetchJson(`/users/me`, 'GET');
-  return fetchRes
+  return fetchRes;
 }
 
 export async function userMePersonal() {
   const fetchRes = await fetchJson(`/users/me/personal`, 'GET');
-  return fetchRes
+  return fetchRes;
 }
-
 
 export async function changePassword(body, option) {
   const fetchRes = await fetchJson(`/users/me/change/password`, 'POST', {
     ...option,
-    body: JSON.stringify(body)
+    body: JSON.stringify(body),
   });
-  return fetchRes
+  return fetchRes;
 }
 
 export async function settingEmail(body, option) {
   const fetchRes = await fetchJson(`/users/me/setting/GENERAL`, 'POST', {
     ...option,
-    body: JSON.stringify(body)
+    body: JSON.stringify(body),
   });
-  return fetchRes
+  return fetchRes;
 }
 
 export async function settingPhoneNumber(body, option) {
   const fetchRes = await fetchJson(`/users/me/change/phone`, 'POST', {
     ...option,
-    body: JSON.stringify(body)
+    body: JSON.stringify(body),
   });
-  return fetchRes
+  return fetchRes;
 }
 
 export async function snsConnect(body, type, option) {
-  if (type === "APPLE") {
+  if (type === 'APPLE') {
     const fetchRes = await fetchJson(`/users/me/connectingApple/`, 'POST', {
       ...option,
-      body: JSON.stringify(body)
+      body: JSON.stringify(body),
     });
-    return fetchRes
+    return fetchRes;
   }
   const fetchRes = await fetchJson(`/users/me/connecting/${type}`, 'POST', {
     ...option,
-    body: JSON.stringify(body)
+    body: JSON.stringify(body),
   });
-  return fetchRes
-
+  return fetchRes;
 }
 export async function cardRegisted(body, option) {
   const fetchRes = await fetchJson(`/users/me/cards`, 'POST', {
     ...option,
-    body: JSON.stringify(body)
+    body: JSON.stringify(body),
   });
-  return fetchRes
+  return fetchRes;
 }
 export async function cardSetting(body, option) {
   const fetchRes = await fetchJson(`/users/me/cards/setting`, 'PATCH', {
     ...option,
-    body: JSON.stringify(body)
+    body: JSON.stringify(body),
   });
-  return fetchRes
+  return fetchRes;
 }
 export async function snsDisconnect(type) {
   const fetchRes = await fetchJson(`/users/me/disconnecting/${type}`, 'DELETE');
-  return fetchRes
+  return fetchRes;
 }
 export async function getCardList() {
   const fetchRes = await fetchJson(`/users/me/cards`, 'GET');
-  return fetchRes
+  return fetchRes;
 }
 
 export async function cardDelete(body, option) {
-  const fetchRes = await fetchJson(`/users/me/cards`, 'DELETE', {
+  const fetchRes = await fetchJson(`/users/me/cards`, 'PATCH', {
     ...option,
-    body: JSON.stringify(body)
+    body: JSON.stringify(body),
   });
-  return fetchRes
+  return fetchRes;
 }
 
 export async function alarmSetting(body, option) {
   const fetchRes = await fetchJson(`/users/me/setting`, 'POST', {
     ...option,
-    body: JSON.stringify(body)
+    body: JSON.stringify(body),
   });
-  return fetchRes
+  return fetchRes;
 }
 
 export async function alarmLookup() {
   const fetchRes = await fetchJson(`/users/me/setting`, 'GET');
-  return fetchRes
+  return fetchRes;
 }
-
