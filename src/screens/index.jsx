848120d73f511de29da04266cd6149fb5c597391
jaesincho <jaesin.cho@dalicious.co>
2023-03-20 15:47:14 +0900

    주석 지우기

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -6,18 +6,8 @@ import React, {useCallback, useEffect} from 'react';
 import {Platform, StatusBar, StyleSheet} from 'react-native';
 import {Alert} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import SplashScreen from 'react-native-splash-screen';
-
-import SignUpComplate, {
-  PAGE_NAME as SignUpComplatePageName,
-} from '~pages/Main/Login/SignUp/SignUpComplate';
-
-import {PAGE_NAME as BuyMealPageName} from '~pages/Main/Bnb/BuyMeal/Main';
-
-import {pageNameAtom} from '../atoms/store';
 
 import Main from './Main';
-import BackButton from '../components/BackButton';
 
 const Root = createNativeStackNavigator();
 
@@ -34,23 +24,6 @@ const Screen = () => {
     <SafeAreaProvider>
       <Root.Navigator>
         <Root.Group screenOptions={{headerShown: false}}>
-          {/* <Root.Screen
-            name={SignUpComplatePageName}
-            component={SignUpComplate}
-            options={{
-              headerShown: true,
-              headerShadowVisible: false,
-              title: '회원가입',
-              headerTitleAlign: 'center',
-              headerTitleStyle: {
-                fontFamily: 'Pretendard-SemiBold',
-                fontSize: 14,
-                lineHeight: 22,
-              },
-              headerLeft: () => <BackButton margin={[10, 0]} />,
-            }}
-          /> */}
-
           <Root.Screen name="ROOT" component={Main} style={styles.rootView} />
         </Root.Group>
       </Root.Navigator>
