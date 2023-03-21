a892c6d8e771c7f6bade4c70255aea772f998de7
jaesincho <jaesin.cho@dalicious.co>
2023-03-21 18:55:37 +0900

    커밋

--- src/pages/Main/MyPage/NoOrder/index.jsx
+++ src/pages/Main/MyPage/NoOrder/index.jsx
@@ -4,24 +4,10 @@ import styled from 'styled-components';
 import Typography from '../../../../components/Typography';
 
 const Component = ({isArrayEmpty, message}) => {
-  console.log('되고있어 빨리확인해');
-  return (
-    <NoOrderText isArrayEmpty={isArrayEmpty}>{message}</NoOrderText>
-    // <>
-    //   <Container>
-    //     <Text>sldkfjslkdfj</Text>
-
-    //   </Container>
-    // </>
-  );
+  return <NoOrderText isArrayEmpty={isArrayEmpty}>{message}</NoOrderText>;
 };
 
 export default Component;
-
-const Container = styled.View`
-  /* flex: 1; */
-  background-color: bisque;
-`;
 
 const NoOrderText = styled(Typography).attrs({text: 'Body05R'})`
   margin: auto;
