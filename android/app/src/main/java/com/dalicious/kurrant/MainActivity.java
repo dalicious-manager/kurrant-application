7c5714a3f33052c644417ebe695e750e3218d187
KyoungTae <117700295+Moyahojang@users.noreply.github.com>
2023-04-10 10:01:04 +0900

    이메일 인증

--- android/app/src/main/java/com/dalicious/kurrant/MainActivity.java
+++ android/app/src/main/java/com/dalicious/kurrant/MainActivity.java
@@ -5,14 +5,13 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import android.os.Bundle;
 import org.devio.rn.splashscreen.SplashScreen;
-import com.dooboolab.naverlogin.RNNaverLoginModule;
 
 
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
-    RNNaverLoginModule.initialize(this);
+    // RNNaverLoginModule.initialize(this);
     SplashScreen.show(this);
   }
 
