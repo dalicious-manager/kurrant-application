5684c73e37ae89af3c303ab26ee69e1442d48949
jaesincho <jaesin.cho@dalicious.co>
2023-04-12 13:32:20 +0900

    커밋

--- src/pages/RegisterInfo/Page5/index.jsx
+++ src/pages/RegisterInfo/Page5/index.jsx
@@ -8,6 +8,7 @@ import {useState} from 'react';
 import ProgressBar from '~components/ProgressBar7';
 
 import {PAGE_NAME as RegisterInfoPage6PageName} from '../Page6';
+import Typography from '~components/Typography';
 
 export const PAGE_NAME = 'P__REGISTER_INFO_PAGE5';
 
@@ -15,6 +16,9 @@ const Pages = () => {
   //   const [clickAvaliable, setClickAvaliable] = useState(false);
 
   const navigation = useNavigation();
+
+  // yes: 1 no: 2
+  const [yesOrNo, setYesOrNo] = useState(0);
 
   const handlePress = () => {
     console.log('ㅗㅑ');
@@ -27,9 +31,30 @@ const Pages = () => {
       styles={{
         position: 'relative',
       }}>
-      <ProgressBar progress={5} />
-      <Text>레지스터 인포 page5</Text>
+      <ScrollViewContainer showsVerticalScrollIndicator={false}>
+        <ProgressBar progress={5} />
 
+        <TitleWrap>
+          <Title>채식 정보</Title>
+          <SemiTitle>평소에 채식을 하시나요?</SemiTitle>
+        </TitleWrap>
+
+        <ButtonContainer>
+          {[
+            {id: 1, name: '예'},
+            {id: 2, name: '아니요'},
+          ].map((v, i) => {
+            return (
+              <SelectButton
+                key={i}
+                data={v}
+                selectedId={yesOrNo}
+                setSelectedId={setYesOrNo}
+              />
+            );
+          })}
+        </ButtonContainer>
+      </ScrollViewContainer>
       <ButtonNext
         size="full"
         label="다음"
@@ -48,10 +73,36 @@ const Container = styled.View`
   flex: 1;
   padding: 35px 20px;
   align-items: center;
-  justify-content: space-between;
+
+  background-color: #ffffff;
+`;
+
+const ScrollViewContainer = styled.ScrollView`
+  width: 100%;
+  /* height: 90%; */
+  background-color: #ffffff;
+`;
+
+const TitleWrap = styled.View`
+  width: 100%;
+  margin-top: 29px;
+  margin-bottom: 24px;
+`;
+const Title = styled(Typography).attrs({text: 'Title04SB'})`
+  color: ${({theme}) => theme.colors.grey[2]};
+  margin-bottom: 8px;
+`;
+const SemiTitle = styled(Typography).attrs({text: 'CaptionR'})`
+  color: ${({theme}) => theme.colors.grey[4]};
+`;
+
+const ButtonContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `;
 
 const ButtonNext = styled(Button)`
-  /* position: absolute; */
-  /* bottom: 35px; */
+  position: relative;
+  bottom: 20px;
 `;
