46758ef21a20ac5e735b0f1bd1e927ef32b8bccc
KyoungTae <117700295+Moyahojang@users.noreply.github.com>
2023-04-17 11:02:03 +0900

    업데이트 체크

--- src/pages/Main/Bnb/More/Main/index.jsx
+++ src/pages/Main/Bnb/More/Main/index.jsx
@@ -149,7 +149,7 @@ const Pages = () => {
       if (applicationList) {
         applicationList();
       }
-      // getData();
+      getData();
     }
   }, []);
   if (isMyInfoLoading && !myInfo) {
