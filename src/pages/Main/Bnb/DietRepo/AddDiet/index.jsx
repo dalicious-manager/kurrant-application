82374c9024c3cff73b3509141ea9ffc8ee9db44b
jaesincho <jaesin.cho@dalicious.co>
2023-05-18 11:33:05 +0900

    식사 추가 rough Ui

--- src/pages/Main/Bnb/DietRepo/AddDiet/index.jsx
+++ src/pages/Main/Bnb/DietRepo/AddDiet/index.jsx
@@ -1,6 +1,10 @@
 import {useNavigation} from '@react-navigation/native';
-import {Pressable, Text} from 'react-native';
-import styled from 'styled-components';
+import {View} from 'react-native';
+import {FlatList, Platform, Text, Pressable} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import styled, {css} from 'styled-components';
+import Button from '~components/Button';
+import DietRepoCard from '../Main/Components/DietRepoCard';
 
 export const PAGE_NAME = 'P_MAIN__DIET_REPO__AddDiet';
 
@@ -9,15 +13,52 @@ import {PAGE_NAME as DietRepoAddMyDietPageName} from '~pages/Main/Bnb/DietRepo/A
 const Pages = ({route}) => {
   const navigation = useNavigation();
 
+  const handlePress = () => {
+    navigation.navigate(DietRepoAddMyDietPageName);
+  };
+
   return (
     <Container>
       <Text>식사 추가</Text>
-      <Pressable
-        onPress={() => {
-          navigation.navigate(DietRepoAddMyDietPageName);
-        }}>
+      <Pressable onPress={() => {}}>
         <Text>내 음식 추가로 가세~~</Text>
       </Pressable>
+
+      <FlatList
+        ListHeaderComponent={
+          <View style={{paddingLeft: 24, paddingRight: 24}}></View>
+        }
+        contentContainerStyle={{paddingBottom: 190}}
+        data={[1, 2, 3, 4]}
+        scrollEnabled={true}
+        renderItem={({item}) => {
+          return (
+            <>
+              <DietRepoCard type="" />
+            </>
+          );
+        }}
+      />
+
+      <ButtonWrapper
+        colors={[
+          'rgba(255, 255, 255, 0)',
+          'rgba(255, 255, 255, 0.3)',
+          'rgba(255, 255, 255, 0.7)',
+          'rgba(255, 255, 255, 0.8048)',
+          'rgba(255, 255, 255, 0.9)',
+          'rgba(255, 255, 255, 0.95)',
+        ]}>
+        <ButtonNext
+          size="full"
+          label="내 음식 추가"
+          text={'BottomButtonSB'}
+          // disabled={!clickAvaliable}
+          onPressEvent={() => {
+            handlePress();
+          }}
+        />
+      </ButtonWrapper>
     </Container>
   );
 };
@@ -29,3 +70,27 @@ const Container = styled.View`
 
   padding: 0px 24px;
 `;
+
+const ButtonWrapper = styled(LinearGradient)`
+  position: relative;
+  ${() => {
+    if (Platform.OS === 'ios') {
+      return css`
+        bottom: 35px;
+      `;
+    } else {
+      return css`
+        bottom: 24px;
+        /* bottom: 1px; */
+      `;
+    }
+  }}
+
+  padding:0 24px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ButtonNext = styled(Button)``;
