c6685aaf20e6de06092641abae3d64d1a9c89f8d
jaesincho <jaesin.cho@dalicious.co>
2023-06-08 14:48:55 +0900

    커밋

--- src/pages/Main/Bnb/DietRepo/useGetDietRepo.jsx
+++ src/pages/Main/Bnb/DietRepo/useGetDietRepo.jsx
@@ -3,16 +3,17 @@ import {useInfiniteQuery, useQuery} from 'react-query';
 import {fetchJson} from '../../../../../../../utils/fetch';
 import {useAtom} from 'jotai';
 
-const useGetDietRepo = () => {
+const useGetDietRepo = (mainDate, addMealDate, addMealDiningType) => {
   useQuery(['dietRepo', 'main'], async ({queryKey}) => {
     const response = await fetchJson(
-      `/users/me/daily/report?date=2023-05-30`,
+      `/users/me/daily/report?date=${mainDate}`,
       'GET',
     );
   });
-  useQuery(['dietRepo', 'main'], async ({queryKey}) => {
+  useQuery(['dietRepo', 'addMeal'], async ({queryKey}) => {
     const response = await fetchJson(
-      `/users/me/daily/report?date=2023-05-30`,
+      `/users/me/daily/report/order?date=${addMealDate}&diningType=${addMealDiningType}`,
+      //   `/users/me/daily/report/order?date=2023-05-30&diningType=2`,
       'GET',
     );
   });
