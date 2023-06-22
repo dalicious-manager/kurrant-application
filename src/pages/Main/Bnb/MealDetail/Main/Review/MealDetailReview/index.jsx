64ca68ee687828666d92258bc768f2881ecb362c
jaesincho <jaesin.cho@dalicious.co>
2023-06-22 14:46:03 +0900

    커밋

--- src/pages/Main/Bnb/MealDetail/Main/Review/MealDetailReview/index.jsx
+++ src/pages/Main/Bnb/MealDetail/Main/Review/MealDetailReview/index.jsx
@@ -136,8 +136,12 @@ const Component = ({imageLocation, foodName, dailyFoodId}) => {
 
   useEffect(() => {
     if (data?.pages) {
-      const {starAverage, isLast, foodId, totalReview, reviewWrite} =
-        data?.pages[0];
+      const {
+        items: {starAverage, isLast, foodId, totalReview, reviewWrite},
+      } = data?.pages[0];
+
+      // const {starAverage, isLast, foodId, totalReview, reviewWrite} =
+      //   data?.pages[0];
 
       setStarAverage(starAverage);
       setIsLast(isLast);
@@ -359,29 +363,28 @@ const Component = ({imageLocation, foodName, dailyFoodId}) => {
         {data?.pages.map((v, i) => {
           return (
             <View key={i}>
-              {v?.items?.length > 0 &&
-                v?.items?.map(item => {
-                  return (
-                    <Card
-                      key={item.reviewId}
-                      dailyFoodId={dailyFoodId}
-                      id={item.reviewId}
-                      userName={item.userName}
-                      item={item}
-                      likeNum={item.good}
-                      isLike={item.isGood}
-                      createDate={item.createDate}
-                      updateDate={item.updateDate}
-                      writtenDate={convertDateFormat1(item.createDate)}
-                      option={item.option}
-                      rating={item.satisfaction}
-                      reviewText={item.content}
-                      imageLocation={item.imageLocation}
-                      forMakers={item.forMakers}
-                      commentList={item.commentList}
-                    />
-                  );
-                })}
+              {v.items?.reviewList.map((item, i2) => {
+                return (
+                  <Card
+                    key={item.reviewId}
+                    dailyFoodId={dailyFoodId}
+                    id={item.reviewId}
+                    userName={item.userName}
+                    item={item}
+                    likeNum={item.good}
+                    isLike={item.isGood}
+                    createDate={item.createDate}
+                    updateDate={item.updateDate}
+                    writtenDate={convertDateFormat1(item.createDate)}
+                    option={item.option}
+                    rating={item.satisfaction}
+                    reviewText={item.content}
+                    imageLocation={item.imageLocation}
+                    forMakers={item.forMakers}
+                    commentList={item.commentList}
+                  />
+                );
+              })}
             </View>
           );
         })}
