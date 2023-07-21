0416f41d538f73fa324313e81b3c4cf64d12d917
jaesincho <jaesin.cho@dalicious.co>
2023-07-21 10:38:10 +0900

    커밋

--- src/pages/Main/Bnb/MealDetail/Main/Review/MealDetailReview/index.jsx
+++ src/pages/Main/Bnb/MealDetail/Main/Review/MealDetailReview/index.jsx
@@ -1,7 +1,13 @@
 import {useFocusEffect, useNavigation} from '@react-navigation/native';
 import {useAtom} from 'jotai';
 import React, {useCallback, useEffect, useState} from 'react';
-import {ActivityIndicator, Dimensions, FlatList, View} from 'react-native';
+import {
+  ActivityIndicator,
+  Dimensions,
+  FlatList,
+  Platform,
+  View,
+} from 'react-native';
 import {Shadow} from 'react-native-shadow-2';
 import {useQueryClient} from 'react-query';
 import styled, {useTheme} from 'styled-components';
@@ -41,7 +47,7 @@ const Component = ({
   const queryClient = useQueryClient();
   const navigation = useNavigation();
 
-  // console.log(dailyFoodId);
+  console.log(dailyFoodId);
 
   // 샘플 대에터
   // const dailyFoodId = 40827;
@@ -217,6 +223,11 @@ const Component = ({
       ),
     );
   };
+
+  useEffect(() => {
+    console.log('allReviewList 확인');
+    console.log(allReviewList);
+  }, [allReviewList]);
 
   return (
     <Container>
@@ -556,6 +567,7 @@ const WrapWrapView = styled.View`
   top: ${({isOn}) => (isOn ? '215px' : '175px')};
   left: 30px;
   z-index: 1;
+  /* border: 1px solid black; */
 `;
 
 const ShadowWrap = styled(Shadow)`
@@ -563,7 +575,12 @@ const ShadowWrap = styled(Shadow)`
 `;
 
 const FilterSelecterWrap = styled.View`
-  width: 84px;
+  width: ${() => {
+    if (Platform.OS === 'android') {
+      return `86px`;
+    }
+    return `84px`;
+  }};
   background-color: #ffffff;
   flex-direction: column;
   align-items: center;
